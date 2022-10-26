57737462e548ee45e144cfb55ab8890cdbbfea8d
SebastianIoanP <petrucsebastian@yahoo.com>
2022-10-26 15:16:55 +0300

    Register

---
+++ app/src/main/java/com/example/giftry/Register.java
@@ -0,0 +1,23 @@
+package com.example.giftry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Register extends Activity{
+    Button b1,b2;
+    EditText ed1,ed2;
+
+    TextView tx1;
+
+}
