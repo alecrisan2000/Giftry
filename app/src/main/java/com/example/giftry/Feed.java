6f7911f57486b0e842def86ac5d1136f55d6906a
alexandracrisan <ale_crisan2000@yahoo.com>
2022-11-02 02:01:33 +0200

    Feed added

---
+++ app/src/main/java/com/example/giftry/Feed.java
@@ -0,0 +1,8 @@
+package com.example.giftry;
+
+import android.app.Activity;
+import android.widget.Button;
+
+public class Feed extends Activity {
+    Button my_profile, friend_profile, add_gift;
+}
