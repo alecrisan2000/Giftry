d5809a9ed547d8fcac1377b78f06e15e6ac0f336
alexandracrisan <ale_crisan2000@yahoo.com>
2023-01-23 08:06:29 +0200

    Final project

--- app/src/main/java/com/example/giftry/Feed.java
+++
@@ -1,8 +0,0 @@
-package com.example.giftry;
-
-import android.app.Activity;
-import android.widget.Button;
-
-public class Feed extends Activity {
-    Button my_profile, friend_profile, add_gift;
-}
